a2bd18b2f753c6bd4ae6476da16ebf8780a04920
prats234 <109065276+prats234@users.noreply.github.com>
2022-07-11 21:43:46 +0530

    Add files via upload

---
+++ switchcase.java
@@ -0,0 +1,34 @@
+public class switchcase
+{
+    public static void main(String[] args)
+    {
+        char day = 'S';
+        {
+            switch (day)
+            {
+                case 'm':
+                    System.out.println("Monday");
+                break;
+                case 'T':
+                    System.out.println("Tuesday");
+                break;
+                case 'w':
+                    System.out.println("wednesday");
+                break;
+                case 't':
+                    System.out.println("Thursday");
+                    break;
+                case 'f':
+                    System.out.println("Friday");
+                    break;
+                case 'S':
+                    System.out.println("Saturday");
+                    break;
+                case 's':
+                    System.out.println("Sunday");
+                    break;
+
+            }
+        }
+    }
+}
